f34da822fd3035b28f8d52cfe6a0c43c3ab4927b
benkiiyo <namianu0@gmail.com>
2025-06-04 00:31:16 +0800

    Update: Add Weather API key

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 const config = {
-    WEATHER_API_KEY: 'YOUR_API_KEY', // Replace with your OpenWeatherMap API key
+    WEATHER_API_KEY: '91e6034c8aef274dd2a6ba1ed845b064', // Replace with your OpenWeatherMap API key
     WEATHER_BASE_URL: 'https://api.openweathermap.org/data/2.5'
 };
